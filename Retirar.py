2d257e7e131520f69724d25afdaaad013a871f9a
matoe <mateomercado234@gmail.com>
2026-03-02 14:07:34 -0500

    Primera version de la funcion retiro

--- Retirar.py
+++ Retirar.py
@@ -1,7 +1,22 @@
  
 def retiro():
-    
-    menu=int(input("Digite la opcion 2 "))
+    menu = int(input("Digite la opcion 2 "))
+    if menu == 2:
+     while True:
+        retiro = int(input("Digite el valor a retirar: "))
+        if retiro <= saldo_inicial:
+            saldo_inicial -= retiro
+            print("Retiro exitoso. Saldo actual:", saldo_inicial)
+            break
+        else:
+            print("Saldo insuficiente, intente otra vez")
+
+retiro()   
+
+
+
+
+
 
 
 
