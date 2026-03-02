531d5c54910865bdfc1c2bf05a983a8f6595aeda
isai <isaicatano1@gmail.com>
2026-03-02 14:17:43 -0500

    primer

---
+++ deposito.py
@@ -0,0 +1,6 @@
+def deposito():
+ deposito = int(input("valor a depositar: "))
+ confirmacion = input("confirma el valor ingresado?: ")
+ if deposito <=0:
+            print("no se puede depositar un valor negativo")
+             
