77469f8fa76210eb5cc4fb2ab164656517ba90dd
isai <isaicatano1@gmail.com>
2026-03-02 14:20:31 -0500

    fase 2

--- deposito.py
+++ deposito.py
@@ -2,5 +2,5 @@ def deposito():
  deposito = int(input("valor a depositar: "))
  confirmacion = input("confirma el valor ingresado?: ")
  if deposito <=0:
-            print("no se puede depositar un valor negativo")
+            print("no se puede depositar un valor negativo") 
              
--- isai.py
+++
@@ -1,6 +0,0 @@
-def deposito():
- deposito = int(input("valor a depositar: "))
- confirmacion = input("confirma el valor ingresado?: ")
- if deposito <=0:
-            print("no se puede depositar un valor negativo")
-             
