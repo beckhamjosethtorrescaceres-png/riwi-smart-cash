7fe1bae7da3a2d60adf2397b3caa8edf213d8b1b
Luis Mejia <lmejiaidep@gmail.com>
2026-03-02 14:20:36 -0500

    Terminada y corregida la funcionalidad de autenticacion y control de intentos

--- autenticaycontroldeintentos.py
+++ autenticaycontroldeintentos.py
@@ -1,20 +1,28 @@
 #Definiendo el nombre de usuario y su contraseña
-user_name:"Riwi"
-user_password: 123
+user_name = "Riwi"
+user_password = 123
 #Bool que valida que el usuario y contraseña
-is_valid: bool = False
+is_valid= False
+is_valid_atte=True
 #Contador de intentos
 attempts= 0
 
-while is_valid == False:
+while is_valid == False and is_valid_atte:
     try:
         name=input("Digite el nombre de usuario: ")
-        password=int(input("Digite su contraseña"))
+        password=input("Digite su contraseña: ")
+        password=int(password)
         if (name==user_name and password==user_password):
-            print("Iniciaste sesión correctamente")
             is_valid=True
+            print("Iniciaste sesión correctamente")
         else:
             print("Usuario y/o Contraseña inválidos")
-
+            attempts=attempts+1
+            print("Te quedan",3-attempts,"intentos")
+        if (attempts==3):
+            is_valid_atte=False
+            print("Superaste la cantidad de intentos permitidos")
     except:
-        print("Contraseña inválida")
+        print("Usuario y/o Contraseña inválidos")
+        attempts=attempts+1
+        print("Te quedan",3-attempts,"intentos")
