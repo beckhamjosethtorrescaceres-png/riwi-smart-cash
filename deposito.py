8b62321650944af548413c123b43e0677404c43a
isai <isaicatano1@gmail.com>
2026-03-02 14:27:37 -0500

    cambios en deposito

--- deposito.py
+++ deposito.py
@@ -2,5 +2,10 @@ def deposito():
  deposito = int(input("valor a depositar: "))
  confirmacion = input("confirma el valor ingresado?: ")
  if deposito <=0:
-            print("no se puede depositar un valor negativo") 
+      print("no se puede depositar un valor negativo")
+ elif confirmacion.lower()=="si":
+     print("su nuevo saldo es: ",saldo + deposito) 
+          
+ else:
+    print("deposito cancelado")
              
