2418c7f49649352d8826e31fdd84c48b5424f577
matoe <mateomercado234@gmail.com>
2026-03-03 13:43:31 -0500

    creando la funcion de consultar saldo

--- consultar_saldo.py
+++ consultar_saldo.py
@@ -1,9 +1,14 @@
+def consultar_saldo(saldo):
+    print("Tu saldo es:", saldo)
 
-Saldo_inicial = 2000 
-menu=int(input("digite la opcion 1 "))
 
-if menu == "1":
+
+      
+
     
-    print ("su saldo inicial"+Saldo_inicial)   
+
+
+
+ 
 
 
