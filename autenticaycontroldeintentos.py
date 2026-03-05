2569f83de3741f7c7cae3c8d335529cc377bdebf
Luis Mejia <lmejiaidep@gmail.com>
2026-03-05 12:40:54 -0500

    Correccion de errores para cuando la contraseña es mala y los intentos ya son superados y volvi una funcion mi codigo

--- autenticaycontroldeintentos.py
+++ autenticaycontroldeintentos.py
@@ -1,31 +1,38 @@
-#Definiendo el nombre de usuario y su contraseña
-user_name = "Riwi"
-user_password = 123
-#Bool que valida que el usuario, contraseña y la cantidad de intentos sea menor que 3
-is_valid= False
-is_valid_atte=True
-#Contador de intentos
-attempts= 0
+def validarusuarioeintentos():
+    #Definiendo el nombre de usuario y su contraseña
+    user_name = "Riwi"
+    user_password = 123
+    #Bool que valida que el usuario, contraseña y la cantidad de intentos sea menor que 3
+    is_valid= False
+    is_valid_atte=True
+    #Contador de intentos
+    attempts= 0
 
-while is_valid==False and is_valid_atte==True:
-    try:
-        name=input("Digite el nombre de usuario: ")
-        password=input("Digite su contraseña: ")
-        password=int(password)
-        if (name==user_name and password==user_password):
-            is_valid=True
-            print("Iniciaste sesión correctamente")
-       
-        else:
+    while is_valid==False and is_valid_atte==True:
+        try:
+            name=input("Digite el nombre de usuario: ")
+            password=input("Digite su contraseña: ")
+            password=int(password)
+            if (name==user_name and password==user_password):
+                is_valid=True
+                print("Iniciaste sesión correctamente")
+                return True
+        
+            else:
+                print("Usuario y/o Contraseña inválidos")
+                attempts=attempts+1
+                print("Te quedan",3-attempts,"intentos")
+
+            if (attempts==3) :
+                print("Superaste la cantidad de intentos permitidos")
+                is_valid_atte= False
+                return False
+        except:
             print("Usuario y/o Contraseña inválidos")
             attempts=attempts+1
             print("Te quedan",3-attempts,"intentos")
-
-        if (attempts==3) :
-            print("Superaste la cantidad de intentos permitidos")
-            is_valid_atte= False
-    except:
-        print("Usuario y/o Contraseña inválidos")
-        attempts=attempts+1
-        print("Te quedan",3-attempts,"intentos")
+            if (attempts==3) :
+                print("Superaste la cantidad de intentos permitidos")
+                is_valid_atte= False
+                return False
 
