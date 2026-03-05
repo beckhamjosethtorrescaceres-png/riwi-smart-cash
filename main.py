63e3acf91a0409b3911e67cca16bab900b8d854f
beckham <beckhamjosethtorrescaceres@gmail.com>
2026-03-05 12:36:58 -0500

    cajer 1.2

--- main.py
+++ main.py
@@ -1,43 +1,14 @@
 from consultar_saldo import consultar_s
 from Retirar import retiro
 from deposito import deposito
+from autenticaycontroldeintentos import validacion
 
+saldo =1000
 saldoretirado=0
 saldodeposito=0
-saldo = 1000
-#Definiendo el nombre de usuario y su contraseña
-user_name = "riwi"
-user_password = 123
-#Bool que valida que el usuario, contraseña y la cantidad de intentos sea menor que 3
-is_valid= False
-is_valid_atte=True
-#Contador de intentos
-attempts= 0
 
-while is_valid==False and is_valid_atte==True:
-    try:
-        name=input("Digite el nombre de usuario: ")
-        password=input("Digite su contraseña: ")
-        password=int(password)
-        if (name==user_name and password==user_password):
-            is_valid=True
-            print("Iniciaste sesión correctamente")
-       
-        else:
-            print("Usuario y/o Contraseña inválidos")
-            attempts=attempts+1
-            print("Te quedan",3-attempts,"intentos")
+validacion = validacion()
 
-        if (attempts==3) :
-            print("Superaste la cantidad de intentos permitidos")
-            is_valid_atte= False
-    except:
-        print("Usuario y/o Contraseña inválidos")
-        attempts=attempts+1
-        print("Te quedan",3-attempts,"intentos")
-        if (attempts==3) :
-            print("Superaste la cantidad de intentos permitidos")
-            is_valid_atte= False      
 
 def mostrar_menu():
     print("\n" + "═" * 45)
