933996f81bc13c59aa6020acfddd8814f97acd7d
mateo <mateomercado234@gmail.com>
2026-03-05 14:15:10 -0500

    Cambios en rama Retirar

--- Retirar.py
+++ Retirar.py
@@ -1,12 +1,17 @@
 def retiro(saldo):
-    while True:
+    verdadero=True
+    while verdadero:
         valor = int(input("Digite el valor a retirar: "))
         if valor <= saldo:
             saldo -= valor
             print("Retiro exitoso. Saldo actual:", saldo)
-            return saldo
+            verdadero=False
         else:
+            verdadero=False
             print("Saldo insuficiente")
+            return saldo 
 
+    
+    
     
     
