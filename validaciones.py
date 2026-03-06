49884b618d7e51398707eb298dc81726356103aa
leonardo-200224 <ayalaperezleonardo24@gmail.com>
2026-03-05 20:23:51 -0500

    valor no negatico diferente a 0

--- validaciones.py
+++ validaciones.py
@@ -1,6 +1,9 @@
 def validar_entrada_numerica(entrada):
     try:
         numero = int(entrada)
+        if numero <= 0:
+            print("ERROR: El número debe ser mayor a 0.")
+            return None
         return numero
     except ValueError:
         print("ERROR: Debe ingresar un número entero válido.")
